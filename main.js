6b552fca5d16d094a769a6f431b00046e580c3a7
awu.l <awu2.00@gmail.com>
2017-03-16 10:41:43 +0800

    try add npapi, not ok

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path')
 var app = require('app');
 var BrowserWindow = require('browser-window');
 var globalShortcut = require('global-shortcut');
@@ -8,67 +9,92 @@ var ipc = require('ipc');
 
 var mainWindow = null;
 var settingsWindow = null;
+app.commandLine.appendSwitch('--enable-npapi')
 
 app.on('ready', function() {
-    if (!configuration.readSettings('shortcutKeys')) {
-        configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+  if (!configuration.readSettings('shortcutKeys')) {
+    configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+  }
+
+const pluginDir = path.join(__dirname, 'plugins')
+
+console.log(pluginDir)
+
+  mainWindow = new BrowserWindow({
+    // height: 700,
+    // width: 368,
+    // frame: false,
+    // resizable: false,
+    'web-preferences': {
+      'plugins': true
+      // 'extra-plugin-dirs': [
+      //     pluginDir,
+      //   pluginDir + 'AudioIntercom.dll',
+      //   pluginDir + 'AudioRender.dll',
+      //   pluginDir + 'D3DX9_43.dll',
+      //   pluginDir + 'ISTask.dll',
+      //   pluginDir + 'NetStream.dll',
+      //   pluginDir + 'npWebVideoKitPlugin.dll',
+      //   pluginDir + 'OpenAL32.dll',
+      //   path.join(pluginDir, 'PlayCtrl.dll'),
+      //   pluginDir + 'StreamTransClient.dll',
+      //   pluginDir + 'SuperRender.dll',
+      //   pluginDir + 'SystemTransform.dll',
+      //   pluginDir + 'WebVideoKitActiveX.ocx'
+      // ]
     }
 
-    mainWindow = new BrowserWindow({
-        frame: false,
-        height: 700,
-        resizable: false,
-        width: 368
-    });
+  });
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+  // mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/app/demo-easy.html');
 
-    setGlobalShortcuts();
+  setGlobalShortcuts();
 });
 
 function setGlobalShortcuts() {
-    globalShortcut.unregisterAll();
+  globalShortcut.unregisterAll();
 
-    var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
-    var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
+  var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
+  var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
 
-    globalShortcut.register(shortcutPrefix + '1', function () {
-        mainWindow.webContents.send('global-shortcut', 0);
-    });
-    globalShortcut.register(shortcutPrefix + '2', function () {
-        mainWindow.webContents.send('global-shortcut', 1);
-    });
+  globalShortcut.register(shortcutPrefix + '1', function() {
+    mainWindow.webContents.send('global-shortcut', 0);
+  });
+  globalShortcut.register(shortcutPrefix + '2', function() {
+    mainWindow.webContents.send('global-shortcut', 1);
+  });
 }
 
-ipc.on('close-main-window', function () {
-    app.quit();
+ipc.on('close-main-window', function() {
+  app.quit();
 });
 
-ipc.on('open-settings-window', function () {
-    if (settingsWindow) {
-        return;
-    }
+ipc.on('open-settings-window', function() {
+  if (settingsWindow) {
+    return;
+  }
 
-    settingsWindow = new BrowserWindow({
-        frame: false,
-        height: 200,
-        resizable: false,
-        width: 200
-    });
+  settingsWindow = new BrowserWindow({
+    frame: false,
+    height: 200,
+    resizable: false,
+    width: 200
+  });
 
-    settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
+  settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
 
-    settingsWindow.on('closed', function () {
-        settingsWindow = null;
-    });
+  settingsWindow.on('closed', function() {
+    settingsWindow = null;
+  });
 });
 
-ipc.on('close-settings-window', function () {
-    if (settingsWindow) {
-        settingsWindow.close();
-    }
+ipc.on('close-settings-window', function() {
+  if (settingsWindow) {
+    settingsWindow.close();
+  }
 });
 
-ipc.on('set-global-shortcuts', function () {
-    setGlobalShortcuts();
+ipc.on('set-global-shortcuts', function() {
+  setGlobalShortcuts();
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,29 @@
 # Accompanying repository for the Electron guide
 
 ![Sound Machine](https://rawgithub.com/bojzi/sound-machine/master/sketch/sound-machine.png)
+
+
+The code I used for testing is:
+
+const path = require('path')
+const {app, BrowserWindow} = require('electron')
+
+app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, 'pepflashplayer.dll'))
+app.commandLine.appendSwitch('ppapi-flash-version', '22.0.0.192')
+
+app.on('window-all-closed', function() {
+  app.quit()
+})
+
+let mainWindow = null
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      plugins: true
+    },
+  })
+  mainWindow.loadURL('http://www.adobe.com/software/flash/about/')
+})
+The arch of the plugin file is x64.
